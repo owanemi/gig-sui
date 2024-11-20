2554f409b72648bc561eca1266ea76353c691ca0
owanemi <owanemi1741@gmail.com>
2024-11-20 19:47:23 +0100

    updated cors for vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 8080;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://gig-sui.vercel.app']
+  origin: ['http://localhost:3000', 'https://gig-sui.vercel.app']
 }));
 
 const db = new sqlite3.Database(path.join(__dirname, 'jobdb.sqlite'), (err) => {
